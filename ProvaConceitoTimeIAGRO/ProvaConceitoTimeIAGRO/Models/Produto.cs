c506b6b04d955c0677b2dd397207197fc5e47728
Ycaro Afonso <ycaroafonso@gmail.com>
2022-12-15 22:03:06 -0400

    Mapeamento dos objetos

---
+++ ProvaConceitoTimeIAGRO/ProvaConceitoTimeIAGRO/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProvaConceitoTimeIAGRO.Models
+{
+    public class Produto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public Specifications specifications { get; set; }
+    }
+}
---
+++ ProvaConceitoTimeIAGRO/ProvaConceitoTimeIAGRO/Models/Specifications.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ProvaConceitoTimeIAGRO.Models
+{
+    public class Specifications
+    {
+        [JsonProperty("Originally published")]
+        public string Originallypublished { get; set; }
+        public string Author { get; set; }
+
+        [JsonProperty("Page count")]
+        public int Pagecount { get; set; }
+        public string Illustrator { get; set; }
+        public List<string> Genres { get; set; }
+    }
+}
